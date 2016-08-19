18ebe439a99faced90eaa1ab935a098d2ade232b
Alim Babazade <alim095@mail.ru>
2016-08-19 20:46:45 +0500

    Prices page

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,20 +48,26 @@ homeExtra();
 function logos(){
 	
 	var imageArray = ['assets/images/logo-4.png', 'assets/images/logo-4.png', 'assets/images/logo-4.png', 'assets/images/logo-4.png', 'assets/images/logo-4.png'];
+	var i=0;
+	var a = setInterval (function time(){
+		$("#about, #pricesTwo .logo div").append("<img>");
+		$("#about, #pricesTwo .logo div img").attr("src",imageArray[i]);
+		//$("#about .logo div img").show();
+		i++;
+		 
+	},2000 )
 
-	for(i=0; i<5; i++){
-		$("#about .logo div").append("<img>");
-		$("#about .logo div img").attr("src",imageArray[i]);
-		$("#about .logo div img").hide();
-		
-		if(i % 2==0){
-			$("#about .logo div img").eq(i).fadeIn(2000);
+	clearInterval (a, 10000)
 
-		}
-		else{
-			$("#about .logo div img").eq(i).fadeOut(2000);
-		}
-	}
+	// for(i=0; i<5; i++){
+	// 	if(i % 2==0){
+	// 		$("#about .logo div img").eq(i).show(2000);
+			
+	// 	}
+	// 	else{
+	// 		$("#about .logo div img").eq(i).hide();
+	// 	}
+	// }
 }
 logos();
 
@@ -182,3 +188,171 @@ function features(){
 	});
 }
 features();
+
+
+
+function profile(){
+	$("#team p").eq(0).hide();
+	$("#team p").eq(1).show();
+	$("#team p").eq(2).hide();
+	$("#profile .profilDate").eq(1).css({
+		"opacity": "1"
+	});
+	$("#profile .profilDate h5").eq(1).css({
+		color: "#1AC6FF"
+	});
+	$("#profile .profilDate span").eq(1).css({
+		color: "#1AC6FF"
+	});
+
+	//ilkin click olunacaq butonlarin alt xetleri
+	$("#profile .profilDate").eq(0).css({
+		borderBottom: "1px solid #DDDDDD"
+	});
+	$("#profile .profilDate").eq(1).css({
+		borderBottom: "1px solid #DDDDDD"
+	});
+	$("#profile .profilDate").eq(2).css({
+		borderBottom: "1px solid #DDDDDD"
+	});
+
+	//click olunma prosesi
+	$("#profile .profilDate").eq(0).click(function(){
+		//Textlerin gorsenmesi ucun
+		$("#team p").eq(0).show();
+		$("#team p").eq(1).hide();
+		$("#team p").eq(2).hide();
+		//uzerlerine click edilende aktiv olmasi ucun ve icinde olan h5 ve spanin rengini deyismesi ucun
+		$("#profile .profilDate").eq(0).css({
+			"opacity": "1"
+		});
+		$("#profile .profilDate h5").eq(0).css({
+			color: "#1AC6FF"
+		});
+		$("#profile .profilDate span").eq(0).css({
+			color: "#1AC6FF"
+		});
+
+		$("#profile .profilDate").eq(1).css({
+			"opacity": ""
+		});
+		$("#profile .profilDate h5").eq(1).css({
+			color: ""
+		});
+		$("#profile .profilDate span").eq(1).css({
+			color: ""
+		});
+
+		$("#profile .profilDate").eq(2).css({
+			"opacity": ""
+		});
+		$("#profile .profilDate h5").eq(2).css({
+			color: ""
+		});
+		$("#profile .profilDate span").eq(2).css({
+			color: ""
+		});
+		//altindaki xettin aktiv olmasi ucun
+		$("#profile .profilDate").eq(0).css({
+			borderBottom: "1px solid #1AC6FF"
+		});
+		$("#profile .profilDate").eq(1).css({
+			borderBottom: "1px solid #DDDDDD"
+		});
+		$("#profile .profilDate").eq(2).css({
+			borderBottom: "1px solid #DDDDDD"
+		});
+	});
+	$("#profile .profilDate").eq(1).click(function(){
+		//Textlerin gorsenmesi ucun
+		$("#team p").eq(0).hide();
+		$("#team p").eq(1).show();
+		$("#team p").eq(2).hide();
+		//uzerlerine click edilende aktiv olmasi ucun ve icinde olan h5 ve spanin rengini deyismesi ucun
+		$("#profile .profilDate").eq(0).css({
+			"opacity": ""
+		});
+		$("#profile .profilDate h5").eq(0).css({
+			color: ""
+		});
+		$("#profile .profilDate span").eq(0).css({
+			color: ""
+		});
+
+		$("#profile .profilDate").eq(1).css({
+			"opacity": "1"
+		});
+		$("#profile .profilDate h5").eq(1).css({
+			color: "#1AC6FF"
+		});
+		$("#profile .profilDate span").eq(1).css({
+			color: "#1AC6FF"
+		});
+		
+		$("#profile .profilDate").eq(2).css({
+			"opacity": ""
+		});
+		$("#profile .profilDate h5").eq(2).css({
+			color: ""
+		});
+		$("#profile .profilDate span").eq(2).css({
+			color: ""
+		});
+		//altindaki xettin aktiv olmasi ucun
+		$("#profile .profilDate").eq(0).css({
+			borderBottom: "1px solid #DDDDDD"
+		});
+		$("#profile .profilDate").eq(1).css({
+			borderBottom: "1px solid #1AC6FF"
+		});
+		$("#profile .profilDate").eq(2).css({
+			borderBottom: "1px solid #DDDDDD"
+		});
+	});
+	$("#profile .profilDate").eq(2).click(function(){
+		$("#team p").eq(0).hide();
+		$("#team p").eq(1).hide();
+		$("#team p").eq(2).show();
+		$("#profile .profilDate").eq(0).css({
+			"opacity": ""
+		});
+		$("#profile .profilDate h5").eq(0).css({
+			color: ""
+		});
+		$("#profile .profilDate span").eq(0).css({
+			color: ""
+		});
+
+		$("#profile .profilDate").eq(1).css({
+			"opacity": ""
+		});
+		$("#profile .profilDate h5").eq(1).css({
+			color: ""
+		});
+		$("#profile .profilDate span").eq(1).css({
+			color: ""
+		});
+		
+		$("#profile .profilDate").eq(2).css({
+			"opacity": "1"
+		});
+		$("#profile .profilDate h5").eq(2).css({
+			color: "#1AC6FF"
+		});
+		$("#profile .profilDate span").eq(2).css({
+			color: "#1AC6FF"
+		});
+		//altindaki xettin aktiv olmasi ucun
+		$("#profile .profilDate").eq(0).css({
+			borderBottom: "1px solid #DDDDDD"
+		});
+		$("#profile .profilDate").eq(1).css({
+			borderBottom: "1px solid #DDDDDD"
+		});
+		$("#profile .profilDate").eq(2).css({
+			borderBottom: "1px solid #1AC6FF"
+		});
+	});
+	//click olunma prosesi
+}
+profile();
